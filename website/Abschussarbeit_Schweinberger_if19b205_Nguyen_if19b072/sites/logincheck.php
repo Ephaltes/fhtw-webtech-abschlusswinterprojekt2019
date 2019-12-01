5268478861190c572574b2fb29f37a5295155927
rothexD <lukas2.schweinberger@gmail.com>
2019-12-01 02:39:28 +0100

    cookie get user 2

--- website/Abschussarbeit_Schweinberger_if19b205_Nguyen_if19b072/sites/logincheck.php
+++ website/Abschussarbeit_Schweinberger_if19b205_Nguyen_if19b072/sites/logincheck.php
@@ -6,17 +6,26 @@ use Model\UserModel;
 
 $model = new UserModel();
 
-if (!empty($_POST)) {
+session_start();
+if (isset($_COOKIE['USERHASH'])) {
+    if ($model->validcookie($_COOKIE['USERHASH'])) {
+        $_SESSION["user"] = $model->GetUserObject();
+        return header("Location: /index.php");
+    }
+    return header("Location: /index.php");
+}
+
+if (!empty($_POST['login'])) {
     if ($model->IsPassword($_POST["username"], $_POST["password"])) {
-        session_start();
         $_SESSION["user"] = $model->GetUserObject();
         if (!empty($_POST['rememberme'])) {
             $model->cookiemonster($_POST['username']);
-                      return  header("Location: /index.php?rem=1");
+            return header("Location: /index.php?rem=1");
         }
-         return header("Location: /index.php");
+        return header("Location: /index.php");
     }
 }
- return header("Location: /login.html");
+
+return header("Location: /login.html");
 
 
