91856c9bac702df52f5b73fa6bbe74e771917011
rothexD <lukas2.schweinberger@gmail.com>
2019-12-02 23:19:51 +0100

    exception handling news

--- website/Abschussarbeit_Schweinberger_if19b205_Nguyen_if19b072/sites/news/news.php
+++ website/Abschussarbeit_Schweinberger_if19b205_Nguyen_if19b072/sites/news/news.php
@@ -1,46 +1,46 @@
 <?php
-if ($xml = simplexml_load_file("data/news/" . base64_decode($_GET["news"]))){
-
+libxml_use_internal_errors(TRUE); // no errors on screen
+if ($xml = simplexml_load_file("data/news/" . base64_decode($_GET["news"]))) {
     $title = $xml->title;
     $thumbnail = $xml->thumbnail;
     $content = $xml->content;
     $content_raw = $xml->content_raw;
 
     list($day, $month, $year, $hour, $minute, $second) = explode("_", $xml->date);
-    $validnews="true";
+    $validnews = "true";
 } else {
-    $validnews="false";
+    $validnews = "false";
 }
 ?>
 
-<?php if($validnews=="true"){?>
-<div class="container">
-    <div class="row">
-        <div class="col-lg-12 mb-2">
+<?php if ($validnews == "true") { ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 mb-2">
 
-            <h1 class="mt-2"><?php echo $title; ?></h1>
+                <h1 class="mt-2"><?php echo $title; ?></h1>
 
-            <p>
-                Erstellt am <?php echo "$day.$month.$year $hour:$minute"; ?>
-            </p>
+                <p>
+                    Erstellt am <?php echo "$day.$month.$year $hour:$minute"; ?>
+                </p>
 
-            <?php if (!empty($thumbnail)) {
+                <?php if (!empty($thumbnail)) {
+                    ?>
+                    <img  src="<?php echo $thumbnail; ?>" class="img-fluid rounded mx-auto d-block">
+                    <?php
+                }
                 ?>
-                <img  src="<?php echo $thumbnail; ?>" class="img-fluid rounded mx-auto d-block">
-                <?php
-            }
-            ?>
-            <hr>
-            <?php echo $content ?>
+                <hr>
+                <?php echo $content ?>
+            </div>
         </div>
     </div>
-</div>
-<?php }else{ ?>
-<div class="container">
-    <p> Sadly we couldnt match any news to the Link you entered</p>
+<?php } else { ?>
+    <div class="container">
+        <p> Sadly we couldnt match any News to the Link you entered</p>
         <a href="index.php">go back!<a>
-</div>
-<?php } ?>
+                </div>
+            <?php } ?>
 
 
 
