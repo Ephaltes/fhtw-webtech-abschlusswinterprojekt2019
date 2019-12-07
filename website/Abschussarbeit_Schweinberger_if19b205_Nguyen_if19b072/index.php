188b1657a4e49b217b415ed60e27ce912bfb549e
Ephaltes <ngoc.lam@live.at>
2019-12-07 23:55:38 +0100

    About page

--- website/Abschussarbeit_Schweinberger_if19b205_Nguyen_if19b072/index.php
+++ website/Abschussarbeit_Schweinberger_if19b205_Nguyen_if19b072/index.php
@@ -36,6 +36,22 @@ if (!isset($_SESSION['cart'])) {
         <link rel="icon" href="img/748989-200.png">
         <?php require_once("sites/lib_include/fontawesome.php"); ?>
 
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://kit.fontawesome.com/34bdaf57ad.js" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+                integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+                crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+                integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+                crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+                integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+                crossorigin="anonymous"></script>
+
+
+
         <title>1nicer shop</title>
     </head>
     <body style="padding-top:40px;">
@@ -84,18 +100,6 @@ if (!isset($_SESSION['cart'])) {
         <?php include('sites/nav_footer/quicklinks.php'); ?>
         <?php include('sites/nav_footer/footer.php'); ?>
 
-        <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://kit.fontawesome.com/34bdaf57ad.js" crossorigin="anonymous"></script>
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-                integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-                integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-                integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
 
     </body>
 </html>
--- website/Abschussarbeit_Schweinberger_if19b205_Nguyen_if19b072/sites/about.php
+++ website/Abschussarbeit_Schweinberger_if19b205_Nguyen_if19b072/sites/about.php
@@ -1,92 +1,76 @@
 <!-- Container (About Section) -->
-<div class="container-fluid mx-2 mb-2">
 
-
-    <div class="row">
-        <div class="col">
-
-            <h2 class="">Unsere Vision</h2>
-
-            <hr>
-        </div>
+<div class="container">
+    <h2>Unsere Vision</h2>
+    <hr>
+    <div>
+        Als Schüler des FH-Technikum Wiens
+        streben wir an, dass Studium in Mindestzeit mit den
+        best möglichen Erfolg abzuschließen.
+        Wir lieben das FH-Technikum
     </div>
-
-    <div class="row">
-            <div class="col-6">
-                Als Schüler des FH-Technikum Wiens
-                streben wir vieles an
-            </div>
-
-        <div class="col-6 my-auto">
-            <div class="embed-responsive-16by9 embed-responsive">
-                <video class="" controls>
-                    <source src="img/samplevideo.mp4" type="video/mp4">
-                    Your browser does not support the video tag.
-                </video>
-            </div>
-        </div>
-    </div>
-
-
-
-    <div class="row">
-        <div class="col">
-
-            <h2>Das Team</h2>
-            <hr>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-
-            <div>
-                <img src="img/teammitglied1.jpg" class="img-fluid float-left pr-2">
-            </div>
-        <div>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
-            leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-            publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </div>
-
-        <div>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
-            leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-            publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </div>
-
-    </div>
-    </div>
-
-    <div class="row">
-        <div class="col">
-            <div>
-                <img src="img/teammitglied1.jpg" class="img-fluid float-right">
-            </div>
-            <div>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-                and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
-                leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
-                with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-                publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </div>
-
-            <div>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-                and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
-                leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
-                with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-                publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </div>
-
-        </div>
-    </div>
-
 </div>
+
+<div class="container mt-2 mt-lg-5">
+    <div class="embed-responsive-16by9 embed-responsive">
+        <video class="" controls>
+            <source src="img/samplevideo.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
+    </div>
+</div>
+
+<div class="container mt-5 ">
+    <div>
+        <h2>Das Team</h2>
+        <hr>
+
+        <div>
+            Wir sind ein seit Jahren eingespieltes Team und sind auf alles Vorbereitet.
+        </div>
+    </div>
+</div>
+
+<div class="container mt-5 mb-5 ">
+    <div id="carouselExampleIndicators" class="carousel slide bg-dark" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active text-center">
+                <img class="carousel-image img-fluid" src="img/Teammitglied1.jpg" alt="First slide">
+                <div class="carousel-caption d-none d-md-block ">
+                        <span class="text-white bg-carousel">Das bin ich, Lukas , mit meiner Bananne </span>
+                </div>
+            </div>
+            <div class="carousel-item text-center">
+            <img class="carousel-image img-fluid" src="img/Teammitglied2.jpg" alt="Second slide">
+            <div class="carousel-caption d-none d-md-block text-white">
+                <span class="text-white bg-carousel">Mein Auto </span>
+            </div>
+        </div>
+        <div class="carousel-item text-center">
+        <img class="carousel-image img-fluid" src="img/Teammitglied1.jpg" alt="Third slide">
+        <div class="carousel-caption d-none d-md-block text-white">
+            <span class="text-white bg-carousel">Me number 2</span>
+        </div>
+    </div>
+</div>
+<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+</a>
+<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+</a>
+</div>
+</div>
+
+<script>
+    $('.carousel').carousel({
+        interval: 5000
+    })
+</script>
