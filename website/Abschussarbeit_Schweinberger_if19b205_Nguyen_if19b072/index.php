817f1ad59f5e880f4f8d747c2347906a0034ce21
rothexD <lukas2.schweinberger@gmail.com>
2019-12-19 00:07:32 +0100

    adblock be gone

--- website/Abschussarbeit_Schweinberger_if19b205_Nguyen_if19b072/index.php
+++ website/Abschussarbeit_Schweinberger_if19b205_Nguyen_if19b072/index.php
@@ -40,14 +40,14 @@ if (!isset($_SESSION['cart'])) {
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
                 integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-                crossorigin="anonymous"></script>
+        crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
                 integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-                crossorigin="anonymous"></script>
+        crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
                 integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-                crossorigin="anonymous"></script>
-
+        crossorigin="anonymous"></script>
+        <script src="js/ads.js"></script>
 
 
         <title>1nicer shop</title>
@@ -70,36 +70,43 @@ if (!isset($_SESSION['cart'])) {
         </header>
 
         <main>
-        <?php
-        if (!empty($_GET['viewme'])) {
-            switch ($_GET['viewme']) {
-                case "About":
-                    include('sites/about.php');
-                    break;
-                case "Impressum":
-                    include('sites/Legal_law/Impressum.php');
-                    break;
-                case "Datenschutz":
-                    include('sites/Legal_law/Datenschutz.php');
-                    break;
-                case "AGB":
-                    include('sites/Legal_law/AGB.php');
-                    break;
-                default:
-                    echo"couldnt match a view";
-                    break;
+            <?php
+            if (!empty($_GET['viewme'])) {
+                switch ($_GET['viewme']) {
+                    case "About":
+                        include('sites/about.php');
+                        break;
+                    case "Impressum":
+                        include('sites/Legal_law/Impressum.php');
+                        break;
+                    case "Datenschutz":
+                        include('sites/Legal_law/Datenschutz.php');
+                        break;
+                    case "AGB":
+                        include('sites/Legal_law/AGB.php');
+                        break;
+                    default:
+                        echo"couldnt match a view";
+                        break;
+                }
+            } else {
+                if (empty($_GET["news"]))
+                    include("sites/news/all_news.php");
+                else
+                    include("sites/news/news.php");
             }
-        } else {
-            if (empty($_GET["news"]))
-                include("sites/news/all_news.php");
-            else
-                include("sites/news/news.php");
-        }
-        ?>
+            ?>
         </main>
 
         <?php include('sites/nav_footer/quicklinks.php'); ?>
         <?php include('sites/nav_footer/footer.php'); ?>
-
+        <script>
+            $(document).ready(function(){
+            if (window.canRunAds === undefined){
+            // adblocker detected, show fallback
+            alert("Please dont use Adblock");
+            }
+            });
+        </script>
     </body>
 </html>
